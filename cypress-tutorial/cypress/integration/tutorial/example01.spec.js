5e24dccab7c8348d88f88bfe239774979273cb80
Astrid Nino <astrid.nio@gmail.com>
2022-07-12 18:43:54 -0500

    class01

---
+++ cypress-tutorial/cypress/integration/tutorial/example01.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+context('Example 01', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+    it('Test #1', () => {
+        //Nothing
+    });
+});
